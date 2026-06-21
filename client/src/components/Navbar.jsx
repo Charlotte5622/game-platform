82e0d07dbb10bcbd7d3f7111150f27ab942f2c7c
Trui <trui@users.noreply.github.com>
2026-06-21 15:41:04 +0800

    fix: remove version number, add white-space: nowrap to all 26+ button classes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,7 +25,6 @@ export default function Navbar() {
     <nav className="navbar">
       <Link to="/lobby" className="navbar-logo">
         🎮 联机游戏平台
-        <span style={{ fontSize: '10px', opacity: 0.6, marginLeft: '4px' }}>v1.1.0</span>
       </Link>
 
       <div className="navbar-right">
